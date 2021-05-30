d9da607059065449d77e2166d05a39d38b2b8283
Nguyen Nguyen Khai <nknguyengl@gmail.com>
2021-05-30 17:49:00 +0700

    Add Vuetify

--- start/routes.ts
+++ start/routes.ts
@@ -23,3 +23,7 @@ import Route from '@ioc:Adonis/Core/Route'
 Route.get('/', async ({ view }) => {
   return view.render('welcome')
 })
+
+Route.get('*', async ({ view }) => {
+  return view.render('welcome')
+}).as('not_found')
